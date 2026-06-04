957ca1ccb5b434d131157b6cc640c4c86609932d
chimpin <cbonardi@ucsc.edu>
2026-06-04 13:01:49 -0700

    Added Skill Tree tester
    temporary tester added to allow keybind activation of ui paths and giving a skill point

---
+++ Assets/Scripts/SkillTree/SkillTreeTester.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SkillTreeTester : MonoBehaviour
+{
+    void Update()
+    {
+        if (Keyboard.current == null)
+        {
+            return;
+        }
+
+        if (Keyboard.current.uKey.wasPressedThisFrame)
+        {
+            SkillTreeManager.Instance.AddSkillPoint();
+
+            Debug.Log("Skill Point Added. Total = " + SkillTreeManager.Instance.skillPoints);
+        }
+
+        if (!SkillTreeManager.Instance.pathChosen)
+        {
+            if (Keyboard.current.iKey.wasPressedThisFrame)
+            {
+                SkillTreeManager.Instance.ChoosePath(ElementPath.Arcane);
+                Debug.Log(SkillTreeManager.Instance.GetDamageType());
+            }
+
+            if (Keyboard.current.oKey.wasPressedThisFrame)
+            {
+                SkillTreeManager.Instance.ChoosePath(ElementPath.Ice);
+                Debug.Log(SkillTreeManager.Instance.GetDamageType());
+            }
+
+            if (Keyboard.current.pKey.wasPressedThisFrame)
+            {
+                SkillTreeManager.Instance.ChoosePath(ElementPath.Fire);
+                Debug.Log(SkillTreeManager.Instance.GetDamageType());
+            }
+        }
+
+        if (SkillTreeManager.Instance.pathChosen)
+        {
+            if (Keyboard.current.iKey.wasPressedThisFrame)
+            {
+                Debug.Log("I pressed");
+            }
+
+            if (Keyboard.current.oKey.wasPressedThisFrame)
+            {
+                Debug.Log("O pressed");
+            }
+
+            if (Keyboard.current.pKey.wasPressedThisFrame)
+            {
+                Debug.Log("P pressed");
+            }
+        }
+    }
+}
